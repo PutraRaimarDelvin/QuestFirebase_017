fc72bbc5cc054b48cd468411188b417c6c59483b
PutraRaimarDelvin <delvinputra805@gmail.com>
2025-01-13 19:03:00 +0700

    menambahkan fungsi untuk getallmhs berdasarkan nim

--- app/src/main/java/com/example/firebase/repository/NetworkRepositoryMhs.kt
+++ app/src/main/java/com/example/firebase/repository/NetworkRepositoryMhs.kt
@@ -1,34 +1,31 @@
 package com.example.firebase.repository
+
 import com.example.firebase.model.Mahasiswa
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.Query
 import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.callbackFlow
+import kotlinx.coroutines.tasks.await
 
-class NetworkRepositoryMhs (
-    private val firestore : FirebaseFirestore
+class NetworkRepositoryMhs(
+    private val firestore: FirebaseFirestore
 ) : RepositoryMhs {
     override suspend fun insertMhs(mahasiswa: Mahasiswa) {
-        TODO("Not yet implemented")
-    }
-
-    override suspend fun deleteMhs(mahasiswa: Mahasiswa) {
-        TODO("Not yet implemented")
-    }
-
-    override suspend fun updateMhs(mahasiswa: Mahasiswa) {
-        TODO("Not yet implemented")
-
+        try {
+            firestore.collection("Mahasiswa").add(mahasiswa).await()
+        } catch (e: Exception) {
+            throw Exception("Gagal menambahkan data mahasiswa : ${e.message}")
+        }
     }
 
     override fun getAllMhs(): Flow<List<Mahasiswa>> = callbackFlow {
         val mhsCollection = firestore.collection("Mahasiswa")
             .orderBy("nim", Query.Direction.ASCENDING)
-            .addSnapshotListener{value, error ->
+            .addSnapshotListener{ value, error ->
                 if (value != null) {
                     val mhsList = value.documents.mapNotNull {
-                        it.toObject(Mahasiswa::class.java)
+                        it.toObject(Mahasiswa::class.java)!!
                     }
                     trySend(mhsList)
                 }
@@ -38,7 +35,4 @@ class NetworkRepositoryMhs (
             mhsCollection.remove()
         }
     }
-    override fun getMhs(nim: String): Flow<Mahasiswa> {
-        TODO("Not Yet implemented")
-    }
-}
+
