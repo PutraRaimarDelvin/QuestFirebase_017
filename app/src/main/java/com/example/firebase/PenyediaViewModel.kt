7d1910e5867c327563be5adcfcd938a1bf1d90f5
PutraRaimarDelvin <delvinputra805@gmail.com>
2025-01-13 19:08:36 +0700

    berfungsi untuk panggilan yang nantinya di panggil di view

--- app/src/main/java/com/example/firebase/PenyediaViewModel.kt
+++ app/src/main/java/com/example/firebase/PenyediaViewModel.kt
@@ -5,15 +5,21 @@ import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.firebase.ui.home.viewmodel.HomeViewModel
+import com.example.firebase.ui.home.viewmodel.InsertViewModel
 
-object PenyediaViewModel{
+
+object PenyediaViewModel {
     val Factory = viewModelFactory {
         initializer {
-            HomeViewModel(
-                MahasiswaApp().container.repositoryMhs)
+            HomeViewModel(MahasiswaApplications().container.repositoryMhs)
+        }
+        initializer {
+            InsertViewModel(MahasiswaApplications().container.repositoryMhs)
         }
     }
+
 }
 
-fun CreationExtras.MahasiswaApp(): MahasiswaApp =
+
+fun CreationExtras.MahasiswaApplications(): MahasiswaApp =
     (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as MahasiswaApp)
