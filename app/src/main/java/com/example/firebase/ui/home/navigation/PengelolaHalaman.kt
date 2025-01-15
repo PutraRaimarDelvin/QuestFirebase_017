9784f584e70773a44fa893e3c9716fb535c2d872
PutraRaimarDelvin <delvinputra805@gmail.com>
2025-01-15 17:07:48 +0700

    menambahkan destinasi detail

--- app/src/main/java/com/example/firebase/ui/home/navigation/PengelolaHalaman.kt
+++ app/src/main/java/com/example/firebase/ui/home/navigation/PengelolaHalaman.kt
@@ -3,9 +3,12 @@ package com.example.firebase.ui.home.navigation
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.example.firebase.ui.home.pages.DetailView
 import com.example.firebase.ui.home.pages.HomeScreen
 import com.example.firebase.ui.home.pages.InsertMhsView
 
@@ -35,5 +38,28 @@ fun PengelolaHalaman(
                 }
             )
         }
+        composable(
+            DestinasiDetail.routeWithArgs,
+            arguments = listOf(
+                navArgument(DestinasiDetail.NIM){
+                    type = NavType.StringType
+                }
+            )
+        ) {
+            val nim = it.arguments?.getString(DestinasiDetail.NIM)
+            nim?.let {
+                DetailView(
+                    onBack = {
+                        navController.navigate(DestinasiHome.route) {
+                            popUpTo(DestinasiHome.route) {
+                                inclusive = true
+                            }
+                        }
+                    },
+
+                    )
+            }
+        }
     }
+
 }
